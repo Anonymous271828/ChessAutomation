9552ccb8a4f2c2a51df41241081c2a91824dfea1
Sol Kwak <141194803+Anonymous271828@users.noreply.github.com>
2024-06-15 18:45:06 -0400

    Update Chess.py

--- Chess.py
+++ Chess.py
@@ -15,7 +15,7 @@ import time
 column = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8}
 def turn(puzzles):
     if puzzles:
-        img = pyautogui.screenshot(r"c:\Users\kiss1\PycharmProjects\Automation\Chess_player\turn.png", region=(1390, 135, 222, 45))
+        img = pyautogui.screenshot(r"", region=(1390, 135, 222, 45))
         img = cv2.imread("turn.png")
         who = cv2.imread("white_turn.png")
         diff = cv2.subtract(img, who)
@@ -36,7 +36,7 @@ def find_fen(game, who, castling, fish, increase):
     pieces = ["wpw", "wnw", "wbw", "wqw", "wkw", "wrw", "bpw", "bnw", "bbw", "bqw", "bkw", "brw", "wpb", "wnb", "wbb", "wqb", "wkb", "wrb", "bpb", "bnb", "bbb", "bqb", "bkb", "brb"]
     for y in range(8):
         for x in range(8):
-            img = pyautogui.screenshot(r"c:\Users\kiss1\PycharmProjects\Automation\Chess_player\screenshot.png", region=(353 + 108 * x, 119 + 108 * y, 108, 108))
+            img = pyautogui.screenshot(r"", region=(353 + 108 * x, 119 + 108 * y, 108, 108))
             img = cv2.imread("screenshot.png")
             for i in pieces:
                 piece = cv2.imread("{}.png".format(i))
@@ -96,49 +96,7 @@ def load_game():
     pyautogui.moveTo(400, 1066)
     pyautogui.click()
 
-#time.sleep(2)
-#img = pyautogui.screenshot(r"c:\Users\kiss1\PycharmProjects\Automation\Chess_player\screenshot.png", region = (288+108*3, 119+108*6, 108, 108))
-#img.save(r"c:\Users\kiss1\PycharmProjects\Automation\Chess_player\brw.png")
-#img.save(r"c:\Users\kiss1\PycharmProjects\Automation\Chess_player")
-#who = turn(True)
-#castling, final_fen = find_fen("f", who, castling)
-#print(final_fen)
-
-# img = pyautogui.screenshot(r"c:\Users\kiss1\PycharmProjects\Automation\Chess_player\screenshot.png", region=(288+108*7, 119+108*7, 108, 108))
-# img = cv2.imread("screenshot.png")
-# piece = cv2.imread("brw.png")
-# diff = cv2.subtract(img, piece)
-# result = not np.any(diff)
-# #print(diff)
-# if result is True:
-#     print(img)
-#     print("Yay")
-# else:
-#     print(img)
-#     print("AAAAA")
-#     print(piece)
-#     print(np.sum(diff**2))
-#     cv2.imshow("gf", diff)
-#     cv2.imshow("gfg", img)
-#     cv2.imshow("gff", piece)
-#     cv2.waitKey(-1)
-# print("trhgrhgrthtyhty")
-        #print(i, loc)
-
-
-    # wp = cv2.imread("wp.png")
-    # bp = cv2.imread("bp.png")
-    # wr = cv2.imread("wr.png")
-    # br = cv2.imread("br.png")
-    # wn = cv2.imread("wn.png")
-    # bn = cv2.imread("bn.png")
-    # wb = cv2.imread("wb.png")
-    # bb = cv2.imread("bb.png")
-    # wq = cv2.imread("wq.png")
-    # bq = cv2.imread("bq.png")
-    # wk = cv2.imread("wk.png")
-    # bk = cv2.imread("bk.png")
-fish = Stockfish(path=r"\Users\kiss1\PycharmProjects\Automation\Chess_player\stockfish-windows-2022-x86-64-avx2.exe", depth = 18, parameters = {"Threads": 3, "Minimum Thinking Time": 3})
+fish = Stockfish(path=r"", depth = 18, parameters = {"Threads": 3, "Minimum Thinking Time": 3})
 castling = {"wk": "K", "bk": "k", "wq":"Q", "bq":"q"}
 load_game()
 time.sleep(2)
